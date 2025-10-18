b2d5e96858693cfa12ecdb45133cd36b9c05536b
Rafik <wox797@gmail.com>
2025-10-18 13:52:24 +0300

    Delete medregconsult.ru directory

--- medregconsult.ru/js/anchor-links.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor =& gt; {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
